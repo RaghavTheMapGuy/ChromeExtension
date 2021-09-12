1f6ab0fa23c52498f9f8ee8d9b3038a8806b99a6
Raghav Lall <z5309071@ad.unsw.edu.au>
2021-09-12 12:56:35 +1000

    Trying to make popup

--- content.js
+++ content.js
@@ -1,24 +1,16 @@
 console.log("Chrome extension go!");
 
-chrome.runtime.onMessage.addListener(gotMessage);
 
-function gotMessage(message, sender, sendResponse) {
-  console.log(message.txt);
+let start = document.getElementById('sb_ifc50').getElementsByTagName("input")[0].getAttribute("aria-label");
+let end = document.getElementById('sb_ifc51').getElementsByTagName("input")[0].getAttribute("aria-label");
 
-  if (message.txt === "calculate-safety") {
-    let start = document.getElementById('sb_ifc50').getElementsByTagName("input")[0].getAttribute("aria-label");
-    let end = document.getElementById('sb_ifc51').getElementsByTagName("input")[0].getAttribute("aria-label");
-    
-    let s = start.replace("Starting point ", "");
-    let e = end.replace("Destination ", "");
-    
-    console.log(s, e);
-    // var s = "ResMed, Innovation Centre, 1 Elizabeth MacArthur Dr, Bella Vista NSW 2153";
-    // var e = "UNSW Library, Library, Kensington NSW 2035"; 
-    makeCall(s, e);
-  }
+let s = start.replace("Starting point ", "");
+let e = end.replace("Destination ", "");
 
-}
+console.log(s, e);
+// var s = "ResMed, Innovation Centre, 1 Elizabeth MacArthur Dr, Bella Vista NSW 2153";
+// var e = "UNSW Library, Library, Kensington NSW 2035"; 
+makeCall(s, e);
 
 function makeCall(start, end) {
   const req = new XMLHttpRequest();
@@ -36,19 +28,19 @@ function makeCall(start, end) {
 
   req.onreadystatechange = function() { // Call a function when the state changes.
     if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-        console.log("Got response 200!");
-        console.log(req.response);
-        response = req.response;
+      console.log("Got response 200!");
+      console.log(req.response);
+      response = req.response;
+      
+      for (i = 0; i < response.length; i++) {
+        console.log(this.response[i]);
+        elm = document.getElementById(`section-directions-trip-title-${i}`);
         
-        for (i = 0; i < response.length; i++) {
-          console.log(this.response[i]);
-            e = document.getElementById(`section-directions-trip-title-${i}`);
-            
-            const newDiv = document.createElement("div");
-            const newContent = document.createTextNode('Safety score: ' + this.response[i]);
-            newDiv.appendChild(newContent);
-            e.appendChild(newDiv);
-        }
+        const newDiv = document.createElement("div");
+        const newContent = document.createTextNode('Safety score: ' + this.response[i]);
+        newDiv.appendChild(newContent);
+        elm.appendChild(newDiv);
+      }
     } else if (this.status !== 200) {
       console.log("Status" + this.status);
     }
